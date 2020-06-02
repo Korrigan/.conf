66c73eec6c756499987b5c69f3acb32a4e9f4eab
Matthieu Rosinski <korrigan@slash16.org>
2020-06-02 12:55:13 +0200

    updated install script

--- .install.zsh
+++ .install.zsh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
-foreach l in `ls .conf`
-ln -s .conf/$l `echo $l | sed -e 's/dot/\./'`
+foreach l in `ls .conf`; do
+  ln -s .conf/$l `echo $l | sed -e 's/dot/\./'`
 end
+
+# Install vscode settings
+if [ -d ~/Library/Application\ Support/Code/User/ ]; then
+  ln -s .conf/dotvscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+  ln -s .conf/dotvscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+fi
